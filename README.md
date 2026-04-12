eb23e6b8aa59177dfa3d62dda33edef79cb1cbf1
Rokokaitos867 <rokokaitos@example.com>
2026-04-12 13:31:50 -0600

    Entrega Specialist Final

---
+++ README.md
@@ -0,0 +1,7 @@
+# Specialist-Audit-Tool
+
+Examen de Specialist
+
+git clone https://github.com/Rokokaitos867/Specialist-Repo
+
+python specialist_crack.py
---
+++ specialist_crack.py
@@ -0,0 +1,8 @@
+echo "# Specialist-Audit-Tool
+
+Examen de Specialist
+
+git clone https://github.com/Rokokaitos867/Specialist-Audit-Tool
+
+python specialist_crack.py" > README.md
+
